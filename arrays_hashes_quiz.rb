be17700c85b5fe91fe100abe4a90eb941ecdc533
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2016-11-23 16:59:20 +0000

    part 3

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -32,7 +32,7 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
-# puts my_hash.reverse_each
+
 
 # 1. How would you return the string `"One"`?
 # puts my_hash[1]
@@ -40,11 +40,15 @@ my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 # 2. How would you return the string `"Two"`?
 # puts my_hash[:two]
 # 3. How would you return the number `2`?
-puts my_hash["two"]
+# puts my_hash["two"]
 
 # 4. How would you add `{3 => "Three"}` to the hash?
+# my_hash[3] = :"Three"
+# puts my_hash
 
 # 5. How would you add `{:four => 4}` to the hash?
+# my_hash[:four] = 4
+# puts my_hash
 
 ### C. Given the following data structure:
 
@@ -81,12 +85,16 @@ users = {
 }
 
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# puts users["Jonathan"][:twitter]
 
 # 2. Return Erik's hometown
+# puts users["Erik"][:home_town]
 
 # 3. Return the array of Erik's favorite numbers
+# puts users["Erik"][:favourite_numbers]
 
 # 4. Return the type of Avril's pet Colin
+# puts users["Avril"][:pets]["colin"]
 
 # 5. Return the smallest of Erik's favorite numbers
 
