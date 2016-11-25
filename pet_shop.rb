e5d83f3f3393636413494f0fd5659c564aec6a2e
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2016-11-25 12:29:22 +0000

    question 3

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,3 +7,8 @@ end
 def total_cash(sum)
 sum = @pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(cash, total_cash)
+total_cash = @pet_shop[:admin][:total_cash] += 10
+cash = total_cash
+end
