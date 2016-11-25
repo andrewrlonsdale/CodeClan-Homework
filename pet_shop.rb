6c366bfcce0abdbd5a6c809406bd0bd247f5f326
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2016-11-25 14:49:01 +0000

    question 7 and 8

--- pet_shop.rb
+++ pet_shop.rb
@@ -16,6 +16,25 @@ def pets_sold(pets_sold)
 return pets_sold[:admin][:pets_sold]
 end
 
-def increase_pets_sold(pets_sold)
-return pets_sold[:admin][:pets_sold] += 2
+def increase_pets_sold(pets_sold, sold)
+return pets_sold[:admin][:pets_sold] += sold
 end
+
+def stock_count(count)
+  return count[:pets].count
+end  
+
+def pets_by_breed(pets, breed)
+total = []
+
+for i in pets[:pets]
+  total.push(i)if i[:breed] == breed
+
+   
+end
+return total
+end
+
+
+
+
