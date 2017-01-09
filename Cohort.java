15e4d40e4e5106691c79e8bf558a87b9dd3bb03b
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-09 23:54:35 +0000

    added 5 passing tests

--- Cohort.java
+++ Cohort.java
@@ -1,15 +1,40 @@
 public class Cohort {
-private String name;
+  private String name;
+  private Students[] room;
 
-public Cohort(String name){
-this.name = name;
+  public Cohort(String name){
+    this.name = name;
+    this.room = new Students[5];
+  }
+  public String getName(){
+    return this.name;
+  }
+
+  public void add(Students person) {
+    if (isRoomFull()) return;
+
+    int personCount = personCount();
+    room[personCount] = person;
+  }
+
+  public int personCount(){
+    int count = 0;
+    for(Students student : room) {
+      if (student != null) {
+        count += 1;
+      }
+    }
+    return count;
+  } 
+
+
+public boolean isRoomFull() {
+  return personCount() == room.length;
 }
-
-public String getName(){
-return this.name;
-}
-
-
-
-
+  
+  public void empty() {
+  for(int i = 0; i < room.length; i++) {
+    room[i] = null;
+    }
+  } 
 }
