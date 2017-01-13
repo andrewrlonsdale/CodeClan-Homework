4c8aa9ac3938d41a1d689a238880e9302808022f
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-13 22:40:02 +0000

    add logic to Card.java

--- 6th week/weekend_homework/card/Card.java
+++ 6th week/weekend_homework/card/Card.java
@@ -1,1 +1,21 @@
 package card;
+
+public class Card {
+
+    private CardSuit suit;
+    private CardType type;
+
+    public Card(CardSuit suit, CardType type) {
+      this.suit = suit;
+      this.type = type;
+    }
+
+    public CardSuit getSuit(){
+      return this.suit;
+    }
+
+    public CardType getType(){
+      return this.type;
+    }
+
+}
