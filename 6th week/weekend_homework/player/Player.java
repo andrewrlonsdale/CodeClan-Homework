9e92ed2397b058f7cb9419a3d530c12afdabb6dd
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-15 13:00:24 +0000

    add logic to Player.java and passing tests to PlayerTest.java

--- 6th week/weekend_homework/player/Player.java
+++ 6th week/weekend_homework/player/Player.java
@@ -1,17 +1,34 @@
 package player;
-
 import java.util.*;
+import card.*;
+
 
 public class Player {
 
   private String name;
+  private Hand hand;
   
 
   public Player(String name){
     this.name = name;
+    this.hand = new Hand();
   }
 
   public String getName(){
     return this.name;
   }
+
+  public Hand getHand(){
+    return this.hand;
+  }
+
+  public void receiveCard(Card card){
+   this.hand.addCard(card);
+ }
+
+ public int showHand(){
+   return this.hand.calculateValue();
+ }
+
+
 }
--- 6th week/weekend_homework/specs/PlayerTest.java
+++ 6th week/weekend_homework/specs/PlayerTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 import player.*;
+import card.*;
+
 
 
 public class PlayerTest {
@@ -16,5 +18,31 @@ public class PlayerTest {
   public void playerCanHaveName() {
     assertEquals("Andrew", player.getName());
   }
+
+  @Test
+  public void canHaveEmptyHand() {
+    Hand hand = player.getHand();
+    assertEquals(0, hand.cardCount());
+  }
+
+  @Test
+  public void canReceiveCard(){
+    Card card1 = new Card(CardSuit.SPADES, CardType.QUEEN);
+    Card card2 = new Card(CardSuit.CLUBS, CardType.KING);
+    player.receiveCard(card1);
+    player.receiveCard(card2);
+    Hand hand = player.getHand();
+    assertEquals(2, hand.cardCount());
+  }
+
+  @Test
+  public void canPlayerShowHand(){
+    Card card1 = new Card(CardSuit.SPADES, CardType.QUEEN);
+    Card card2 = new Card(CardSuit.CLUBS, CardType.KING);
+    player.receiveCard(card1);
+    player.receiveCard(card2);
+    assertEquals(27, player.showHand());
+  }
+
 
 }
