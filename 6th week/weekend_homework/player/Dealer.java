d8729a7e61692583455922a3fb373f9394041cb0
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-01-15 13:07:54 +0000

    add logic to Dealer.java and test to DealerTest.java

--- 6th week/weekend_homework/player/Dealer.java
+++ 6th week/weekend_homework/player/Dealer.java
@@ -1,1 +1,9 @@
 package player;
+
+public class Dealer extends Player {
+
+  public Dealer(String name){
+    super(name);
+  }
+
+}
---
+++ 6th week/weekend_homework/specs/DealerTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import player.*;
+import card.*;
+
+public class DealerTest {
+
+  Dealer dealer;
+
+  @Before 
+  public void before() {
+    dealer = new Dealer("Bob");
+  }
+
+  @Test
+  public void canHaveName() {
+    assertEquals("Bob", dealer.getName());
+  }
+
+  @Test
+  public void canHaveEmptyHand() {
+    Hand hand = dealer.getHand();
+    assertEquals(0, hand.cardCount());
+  }
+
+  @Test
+  public void canReceiveCard(){
+    Card card1 = new Card(CardSuit.SPADES, CardType.QUEEN);
+    Card card2 = new Card(CardSuit.CLUBS, CardType.KING);
+    dealer.receiveCard(card1);
+    dealer.receiveCard(card2);
+    Hand hand = dealer.getHand();
+    assertEquals(2, hand.cardCount());
+  }
+
+  @Test
+  public void canDealerShowsHand(){
+    Card card1 = new Card(CardSuit.SPADES, CardType.QUEEN);
+    Card card2 = new Card(CardSuit.CLUBS, CardType.KING);
+    dealer.receiveCard(card1);
+    dealer.receiveCard(card2);
+    assertEquals(27, dealer.showHand());
+  }
+
+ 
+}
