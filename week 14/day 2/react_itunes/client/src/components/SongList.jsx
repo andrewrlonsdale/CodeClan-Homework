ac0d6ec5307b40febd0d2c46a881400502c12c61
Andrewrlonsdale <Andrewrlonsdale@hotmail.com>
2017-03-08 09:14:54 +0000

    add day 2 homework

---
+++ week 14/day 2/react_itunes/client/src/components/SongList.jsx
@@ -0,0 +1,26 @@
+var React = require('react');
+
+
+var SongList = function(props) {
+
+  var options = props.songs.map(function(song, index){
+    return (
+     <ul>
+     <li value={index} key={song["im:name"].label}>No.{index+1}</li>
+    <li value={index} key={index}>Title:{song["im:name"].label}</li>
+    <li value={index} key={song.title.label}>Artist:{song["im:artist"].label}</li>
+    <img src={song["im:image"][2].label}></img>
+    </ul>
+      );
+
+  });
+  return (
+    <div id="song-list">
+    {options}
+    </div>
+    )
+};
+
+
+
+module.exports = SongList;
---
+++ week 14/day 2/react_itunes/client/src/containers/SongsContainer.jsx
@@ -0,0 +1,45 @@
+var React = require('react');
+
+var SongList = require('../components/SongList.jsx');
+
+
+var SongContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      songs: []
+    };
+  },
+
+  componentDidMount: function(){
+    var url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = function() {
+      if (request.status === 200){
+        var data = JSON.parse(request.responseText);
+        this.setState({ songs: data.feed.entry});
+      }
+    }.bind(this);
+    request.send();
+
+  },
+
+render: function() {
+  return (
+    <div className="songContainer">
+      <h1>UK Top 20</h1>
+      <SongList songs={this.state.songs} />
+  
+     
+    </div>
+
+    );
+}
+
+});
+
+
+
+
+
+module.exports = SongContainer;
